fdc6807a5ec49f4fb55c34eeb1328cf361f67a5b
Ilyas Andika <andikailyas01@gmail.com>
2025-10-13 07:16:16 +0700

    fix: translate login page and add loading animation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import show from "../assets/icons/show.svg";
-import hide from "../assets/icons/hide.svg";
-import axios from "axios";
 import AuthServices from "../services/AuthServices.js";
-import TextBox from "../Components/Form/TextBox.jsx";
 import {useErrors} from "../hooks/useErrors.jsx";
+import {useTranslation} from "react-i18next";
+import {capitalize} from "../utils/helper.js";
+import {EyeSlashIcon, EyeIcon} from "@heroicons/react/24/solid/index.js";
+import Loading from "../Components/Utils/Loading.jsx";
 
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
@@ -12,10 +12,12 @@ const Login = () => {
     const [employeeId, setEmployeeId] = useState("");
     const [password, setPassword] = useState("");
     const {fieldErrors, generalError, setErrors, clearErrors} = useErrors();
+    const [loading, setLoading] = useState(false);
+    const {t} = useTranslation();
 
     const handleLogin = async (e) => {
         e.preventDefault();
-
+        setLoading(true);
         try {
             const response = await AuthServices.login({ employeeId, password });
             const token = response.data.payload.token;
@@ -30,21 +32,24 @@ const Login = () => {
             window.location.href = "/dashboard";
         } catch (err) {
             setErrors(err);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
 
-        <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 overflow-y-hidden">
             <div className="bg-white shadow-xl w-10/12 max-w-5xl flex">
                 <div className="bg-background w-7/12 h-full px-10 pb-20 pt-40">
                     <img src="/images/loginpage.svg" className="w-8/10 mx-auto" alt="Login Illustration" />
                 </div>
 
-                <div className="bg-white w-5/12 h-full px-10 pt-25 flex flex-col gap-8">
+                <div className="bg-white w-5/12 h-full  px-10 my-auto flex flex-col gap-8 ">
+                    {/*<img src="/images/LOGO_ALHAYAH.png" alt={"logo alhayah"} className="w-40 mx-auto" />*/}
                     <div className="flex flex-col gap-2">
-                        <h1 className="text-center font-bold text-3xl">Welcome to</h1>
-                        <h2 className="text-center">Please enter your credentials</h2>
+                        <h1 className="text-center font-bold text-3xl">{capitalize(t("welcome"))}</h1>
+                        <h2 className="text-center">{capitalize(t("please enter your credentials"), false)}</h2>
                     </div>
 
                     <form onSubmit={handleLogin}>
@@ -54,7 +59,7 @@ const Login = () => {
                                 <input
                                     type="text"
                                     name="employeeId"
-                                    placeholder="Employee ID"
+                                    placeholder={capitalize(t("employee ID"))}
                                     className={`w-full border-b border-primary p-2 font-semibold focus:outline-none placeholder-primary/80 focus:ring-0 ${fieldErrors?.employeeId && " border-red-500" }`}
                                     onChange={(e) => {
                                         setEmployeeId(e.target.value);
@@ -74,7 +79,7 @@ const Login = () => {
                                     <input
                                         type={showPassword ? "text" : "password"}
                                         name="password"
-                                        placeholder="Password"
+                                        placeholder={capitalize(t("password"))}
                                         className="border-b border-primary p-2 w-full font-semibold focus:outline-none placeholder-primary/80 focus:ring-0 pr-10"
                                         onChange={(e) => {
                                             setPassword(e.target.value);
@@ -87,7 +92,11 @@ const Login = () => {
                                         type="button"
                                         className="absolute right-2 top-1/2 -translate-y-1/2"
                                         onClick={() => setShowPassword(!showPassword)}>
-                                        <img src={showPassword ? show : hide} className="w-6 h-6" alt="Toggle Password Visibility" />
+                                        {
+                                            showPassword ?
+                                                <EyeIcon className="w-5 h-5" /> :
+                                                <EyeSlashIcon className="w-5 h-5" />
+                                        }
                                     </button>
                                 </div>
 
@@ -111,17 +120,20 @@ const Login = () => {
                                         onChange={() => setRememberMe(!rememberMe)}
                                         className="w-4 h-4 accent-primary"
                                     />
-                                    Remember Me
+                                    {capitalize(t("remember me"))}
                                 </label>
                                 <a href="#" className="text-primary hover:underline">
-                                    Forgot Password?
+                                    {capitalize(t("forgot password")) + "?"}
                                 </a>
                             </div>
 
                             <button
                                 type="submit"
                                 className="w-full cursor-pointer bg-primary text-white font-semibold py-3 rounded-4xl hover:bg-primary/80 transition">
-                                Login
+                                {loading ?
+                                    <Loading size ={24}/>:
+                                    "Login"
+                                }
                             </button>
                         </div>
                     </form>
